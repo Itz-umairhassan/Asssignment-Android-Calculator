8ff3e432d8ab7f4b141969d71f9ed1e57989d52b
umair hassan <umairhassangondal3@gmail.com>
2023-04-17 16:23:59 +0500

    Field sanitization and addition method

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,12 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button add_btn,sub_btn,mult_btn;
+    EditText field1,field2;
+
+    protected String calculation(String value1,String value2,int operator){
+     // take both values and first of all check that they are proper numbers...
+        long a,b;
+        try{
+            a=Long.parseLong(value1);
+        }catch (NumberFormatException ex){
+            return "Field 1 should contain a number";
+        }
+
+        try{
+            b=Long.parseLong(value2);
+        }catch(NumberFormatException ex){
+            return "Field 2 should contain a number";
+        }
+
+        // now apply the operation...
+
+        if(operator==1)
+            return ""+(a+b);
+        return "no way";
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +43,16 @@ public class MainActivity extends AppCompatActivity {
 
         add_btn=findViewById(R.id.button4);
 
+
+        field1=findViewById(R.id.editTextTextPersonName2);
+        field2=findViewById(R.id.editTextTextPersonName);
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Addition is being clicked", Toast.LENGTH_SHORT).show();
+                String value1=field1.getText().toString();
+                String value2=field2.getText().toString();
+
+                Toast.makeText(MainActivity.this, calculation(value1,value2,1), Toast.LENGTH_SHORT).show();
             }
         });
     }
